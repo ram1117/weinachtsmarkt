456c740edce55a9ba20c33e25cbe215d80ca1b15
ram1117 <ramkumark1@outlook.com>
2023-12-14 23:03:57 +0530

    setup husky prehook and lint staged fro eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1>Weinachtsmarkt</h1>
+      <h2>Weinachtsmarkt</h2>
     </main>
   )
 }
